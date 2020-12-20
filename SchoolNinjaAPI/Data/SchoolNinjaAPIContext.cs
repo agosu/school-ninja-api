eeeba5556664090ccf3062ab3c1b40097e0ce962
Agota <45971426+agosu@users.noreply.github.com>
2020-12-20 17:17:32 +0200

    Initial

---
+++ SchoolNinjaAPI/Data/SchoolNinjaAPIContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolNinjaAPI.Models;
+
+namespace SchoolNinjaAPI.Data
+{
+    public class SchoolNinjaAPIContext : DbContext
+    {
+        public SchoolNinjaAPIContext (DbContextOptions<SchoolNinjaAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ SchoolNinjaAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolNinjaAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Firstname { get; set; }
+
+        [Required]
+        public string Lastname { get; set; }
+    }
+}
